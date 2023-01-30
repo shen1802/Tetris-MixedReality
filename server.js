9fe8b479568bebc277f08184c75d7386c273b993
HaohaoShen <shihaosh@ucm.es>
2023-01-30 02:10:15 +0100

    cambio de ws por socket.io

--- server.js
+++ server.js
@@ -1,49 +1,35 @@
-//import {Tetris} from './public/tetris.js'
-
 const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
 const port = 3000;
-const WebSocket = require('ws');
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const wss = new WebSocket.Server({port:3001});
+//socket.io
 
-// establecer la conexión
-wss.on('connection', function connection(ws) {
+io.on('connection', (socket) => {
+  console.log('Un nuevo usuario se ha conectado');
   
-  console.log("Se ha conectado un nuevo usuario");
-  ws.on('message', function message(data) {
-    console.log('Server received: %s', data);
-    if (data == 'mr') {
-      
-    } else if(data == 'start') {
-      
-    }
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
-  ws.send('Bienvenido al servidor del tetris');
+
+  socket.on('mensaje', function(mensaje){
+    console.log('mensaje: ' + mensaje);
+    io.emit('mensaje', mensaje);
+
+});
 });
 
-app.use(express.static(__dirname + "/public"));
+//express
 
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/tetris.html");
 });
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log(`Tetris app listening on port ${port}`);
 });
 
-
-/*--FUNCIONA EL WEBSOCKET
-//WebSocket
-const { Server } = require('ws');
-
-const sockserver = new Server({ port: 3001 });
-sockserver.on('connection', (ws) => {
-   console.log('New client connected!');
-   ws.send("Welcome to the tetris server!");
-   ws.on('close', () => console.log('Client has disconnected!'));
-});
-
-*/
