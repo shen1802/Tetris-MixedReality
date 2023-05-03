0c24ff2dc6926546a5a406de43f72c078957de99
HaohaoShen <shihaosh@ucm.es>
2023-05-03 12:34:42 +0200

    Changed bbdd

--- testing.sql
+++ testing.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 25-04-2023 a las 22:50:41
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2023 at 10:04 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `testing`
+-- Database: `testing`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `board`
+-- Table structure for table `board`
 --
 
 CREATE TABLE `board` (
@@ -33,7 +33,12 @@ CREATE TABLE `board` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `board`
+-- Truncate table before insert `board`
+--
+
+TRUNCATE TABLE `board`;
+--
+-- Dumping data for table `board`
 --
 
 INSERT INTO `board` (`id`, `taken`) VALUES
@@ -42,26 +47,35 @@ INSERT INTO `board` (`id`, `taken`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `institution`
+-- Table structure for table `institution`
 --
 
 CREATE TABLE `institution` (
-  `id` int(11) NOT NULL,
+  `id` int(4) NOT NULL,
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `institution`
+-- Truncate table before insert `institution`
+--
+
+TRUNCATE TABLE `institution`;
+--
+-- Dumping data for table `institution`
 --
 
 INSERT INTO `institution` (`id`, `name`) VALUES
-(7, 'Rey Juan Carlos'),
-(1, 'UCM');
+(0, 'admin'),
+(6458, 'Universidad Autónoma de Madrid'),
+(5242, 'Universidad Carlos III de Madrid'),
+(4203, 'Universidad Complutense de Madrid'),
+(5522, 'Universidad Politécnica de Madrid'),
+(5812, 'Universidad Rey Juan Carlos');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `role`
+-- Table structure for table `role`
 --
 
 CREATE TABLE `role` (
@@ -70,7 +84,12 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `role`
+-- Truncate table before insert `role`
+--
+
+TRUNCATE TABLE `role`;
+--
+-- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`id`, `description`) VALUES
@@ -81,7 +100,7 @@ INSERT INTO `role` (`id`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `session`
+-- Table structure for table `session`
 --
 
 CREATE TABLE `session` (
@@ -91,10 +110,39 @@ CREATE TABLE `session` (
   `session_score` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Truncate table before insert `session`
+--
+
+TRUNCATE TABLE `session`;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `study_group`
+--
+
+CREATE TABLE `study_group` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `institution_id` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `study_group`
+--
+
+TRUNCATE TABLE `study_group`;
+--
+-- Dumping data for table `study_group`
+--
+
+INSERT INTO `study_group` (`id`, `name`, `institution_id`) VALUES
+(2147483647, '1ro de Carrera', 4203);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -104,67 +152,84 @@ CREATE TABLE `user` (
   `age` int(10) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(10) DEFAULT NULL,
-  `institution_id` int(11) DEFAULT NULL
+  `institution_id` int(11) DEFAULT NULL,
+  `study_group_id` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `user`
+-- Truncate table before insert `user`
 --
 
-INSERT INTO `user` (`username`, `name`, `surname`, `age`, `password`, `role`, `institution_id`) VALUES
-('admin', 'admin', 'admin', 0, 'admin', 1, 1),
-('Siao', 'Shihao', 'S', 20, '1234', 3, 1);
-
+TRUNCATE TABLE `user`;
 --
--- Índices para tablas volcadas
+-- Dumping data for table `user`
 --
 
+INSERT INTO `user` (`username`, `name`, `surname`, `age`, `password`, `role`, `institution_id`, `study_group_id`) VALUES
+('admin', 'admin', 'admin', 0, 'admin', 1, 0, NULL),
+('profe', 'Tamayo', 'Salas', 32, '1234', 2, 4203, NULL),
+('Siao', 'Shihao', 'Shen', 23, '1234', 3, 4203, NULL);
+
 --
--- Indices de la tabla `board`
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `board`
 --
 ALTER TABLE `board`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `institution`
+-- Indexes for table `institution`
 --
 ALTER TABLE `institution`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `institution_pk` (`name`);
 
 --
--- Indices de la tabla `role`
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `session`
+-- Indexes for table `session`
 --
 ALTER TABLE `session`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `user`
+-- Indexes for table `study_group`
+--
+ALTER TABLE `study_group`
+  ADD KEY `study_group_institution_id_fk` (`institution_id`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`username`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `institution`
---
-ALTER TABLE `institution`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `session`
+-- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `study_group`
+--
+ALTER TABLE `study_group`
+  ADD CONSTRAINT `study_group_institution_id_fk` FOREIGN KEY (`institution_id`) REFERENCES `institution` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
